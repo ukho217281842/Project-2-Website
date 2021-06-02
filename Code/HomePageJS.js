68b0961b991d3552b853bf04b0b1ee8e0e486257
DominicPrzygonski <domprzygonski@gmail.com>
2021-06-02 17:57:04 +0200

    Creating animated slogan

--- Code/HomePageJS.js
+++ Code/HomePageJS.js
@@ -114,3 +114,20 @@ $('.counter').each(function () {
       }
     });
 });
+
+
+//Changing words effect for slogan
+setInterval(function () {
+  const show = document.querySelector('span[data-show]')
+  const next = show.nextElementSibling || document.querySelector('span:first-child')
+  const up = document.querySelector('span[data-up]')
+  
+  if (up) {
+    up.removeAttribute('data-up')
+  }
+  
+  show.removeAttribute('data-show')
+  show.setAttribute('data-up', '')
+  
+  next.setAttribute('data-show', '')
+}, 2000)
